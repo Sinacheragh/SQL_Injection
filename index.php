3ea921208175745bce9f7d13e6b2c627d4fd5078
Sinacheragh <sinacheraqi1@gmail.com>
2025-10-12 22:47:35 +0330

    m

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "<h1>Hello from PHP on Render!</h1>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    salam
+</body>
+</html>
